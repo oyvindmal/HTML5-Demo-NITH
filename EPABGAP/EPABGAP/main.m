22182c8456e77bc5ea045dea910fd4bdf818821b
Oyvind Malin <oyvind.malin@bekk.no>
2011-09-22 13:54:36 +0200

    Added phonegapsample

---
+++ EPABGAP/EPABGAP/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  EPABGAP
+//
+//  Created by Øyvind Malin on 9/22/11.
+//  Copyright Oyvindm 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
